67517c0a9bd74d24841e3d452ab09889ceafc715
deq406 <egormartin0@gmail.com>
2022-10-19 16:35:49 +0300

    added NotFoundPage

--- src/pages/NotFoundPage/index.jsx
+++ src/pages/NotFoundPage/index.jsx
@@ -13,13 +13,12 @@ export const NotFoundPage = () => {
         alignItems: "center",
         flexDirection: "column",
         minHeight: "100vh",
-        backgroundColor: primary,
       }}
     >
-      <Typography variant="h1" style={{ color: "white" }}>
+      <Typography variant="h1" style={{ color: "black" }}>
         404
       </Typography>
-      <Typography variant="h6" style={{ color: "white" }}>
+      <Typography variant="h6" style={{ color: "black" }}>
         The page you’re looking for doesn’t exist.
       </Typography>
     </Box>
